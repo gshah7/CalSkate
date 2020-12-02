5a483d5e149f48ed1388fd89c77e0ca497ae62c1
Gaurav Shah <gauravs7@berkeley.edu>
2020-12-02 00:02:16 -0800

    Initial Commit

---
+++ CalSkate/CalSkateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalSkateApp.swift
+//  CalSkate
+//
+//  Created by Gaurav Shah on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct CalSkateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
